bc43a5cf91fc75b51e5fbf37996f082ad6b96da0
Matthew Isnor <matt@isnor.io>
2019-07-12 21:36:51 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# BabsECS
+
+Entity Component System implemented in C++, ideally with decent error handling and acceptable performance.
+
+## Build and Run
+1. mkdir build
+2. cd build
+3. cmake ..
+4. make
+5. ./babsecs
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "temp/math.hpp"
+
+int main() {
+	std::cout << "BabsECS, Hello World!" << std::endl;
+	std::cout << "temporary math operation add(2, 5) = " << math::add(2, 5) << std::endl;
+	return 0;
+}
