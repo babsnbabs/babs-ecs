689f53770297fc31b930063855e2bf17e84a7d0e
Matthew Isnor <matt@isnor.io>
2019-08-08 19:00:32 -0300

    more unit tests

--- src/pubsub/eventmanager_tests.cpp
+++ src/pubsub/eventmanager_tests.cpp
@@ -13,18 +13,46 @@ public:
 	Example(int payload) : payload(payload) {}
 };
 
+struct ExampleObserver {
+	int eventCount = 0;
+	int lastPayloadReceived = 0;
+	void HandlExample(const std::any& e)
+	{
+		Example eventData = std::any_cast<Example>(e);
+		this->lastPayloadReceived = eventData.payload;
+		eventCount++;
+	}
+};
+
+
 TEST_SUITE("Event Manager")
 {
-	TEST_CASE("EventManager can subscribe and broadcast events")
-	{
-		int expectedPayload = 111;
-		EventManager eventManager;
+	EventManager eventManager;
+	int expectedPayload = 111;
 
+	TEST_CASE("EventManager can subscribe and broadcast and event to an anonymous function")
+	{
 		eventManager.Subscribe<Example>([&](const std::any& e) {
 			Example eventData = std::any_cast<Example>(e);
 			REQUIRE(eventData.payload == 111);
-			std::cout << "yeah babs, it delivered the event data: " << eventData.payload << std::endl;
 		});
+		eventManager.Broadcast<Example>(Example(111));
+	}
+
+	TEST_CASE("EventManager can subscribe and broadcast to a function in a class")
+	{
+		ExampleObserver observer;
+		REQUIRE(observer.eventCount == 0);
+
+		eventManager.Subscribe<Example>(std::bind(&ExampleObserver::HandlExample, &observer, std::placeholders::_1));
+		eventManager.Broadcast<Example>(Example(111));
+
+		REQUIRE(observer.eventCount == 1);
+		REQUIRE(observer.lastPayloadReceived == expectedPayload);
+	}
+
+	TEST_CASE("EventManager can broadcast an event no one is listening to")
+	{
 		eventManager.Broadcast<Example>(Example(111));
 	}
 }
